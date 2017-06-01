aab4b9c855a34f83fc64353fdfa95d667d505c04
Domenico Rescigno <domenico.rescigno@gmail.com>
2017-06-02 00:12:35 +0200

    Working on the AddARemotePage

--- mrRemoteForKodi/mrRemoteForKodi/Views/AddARemotePage.xaml.cs
+++ mrRemoteForKodi/mrRemoteForKodi/Views/AddARemotePage.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.ComponentModel;
+﻿using mrRemoteForKodi.Services;
+
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace mrRemoteForKodi.Views
@@ -26,5 +29,16 @@ namespace mrRemoteForKodi.Views
         }
 
         private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private void ButtonBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (NavigationService.CanGoBack)
+                NavigationService.GoBack();
+        }
+
+        private void HyperlinkHelp_Click(Windows.UI.Xaml.Documents.Hyperlink sender, Windows.UI.Xaml.Documents.HyperlinkClickEventArgs args)
+        {
+            FlyoutHelp.ShowAt(TextBlockHelp);
+        }
     }
 }
